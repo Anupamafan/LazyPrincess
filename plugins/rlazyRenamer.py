0ffc7347ca6dc4b37bfaf06e8ed36f7fc4ab0616
Anupamafan <118895531+Anupamafan@users.noreply.github.com>
2023-09-12 17:17:16 +0530

    Update rlazyRenamer.py

--- plugins/rlazyRenamer.py
+++ plugins/rlazyRenamer.py
@@ -16,8 +16,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-Dev Channel Link : https://t.me/LazyDeveloper 
-Repo Link : https://github.com/LazyDeveloperr/LazyPrincess
+Dev Channel Link : https://t.me/Movies_Telugu_Top 
+Repo Link : https://t.me/Movies_Telugu_Top
 License Link : https://github.com/LazyDeveloperr/LazyPrincess/blob/main/LICENSE
 # Removing this is strictly prohibited ! Don't remove this all without the 
 permission of LazyDeveloperr
